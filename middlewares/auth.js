37b871db15b26fdddf000c7df29e4cbdfe40e9a3
Jh_Moshnikov <Jh.Moshnikov@yandex.ru>
2023-06-21 23:07:33 +0300

    pr14 1.1.3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const AuthError = require('../utils/errors/authError');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
+    return next(new AuthError('Необходима авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
